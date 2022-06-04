d4f2af3fa89e0c5c05a109fd5b2706fa28f9b1b3
Megan McKissack <meganmckissack@gmail.com>
2022-06-03 21:13:16 -0700

    update CurrencyService static method parameter and api url

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,6 +1,8 @@
 export class CurrencyService {
-  static async convertCurrency() {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+  static async convertCurrency(currencyCode) {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyCode}`);
     return response.json();
   }
 }
+
+// next step - add error handling 
