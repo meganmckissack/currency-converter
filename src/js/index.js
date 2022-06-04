b7dd03bb1450c75c41c8e342af160d58e75e84d5
Megan McKissack <meganmckissack@gmail.com>
2022-06-03 20:52:43 -0700

    import CurrencyService and create function to call api based on currency code input and get response

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
+import { CurrencyService } from './currency-service.js';
 
+async function conversionCall(currencyCode) {
+  const response = await CurrencyService.convertCurrency(currencyCode);
+  console.log(response);
+}
 
 $(document).ready(function() {
   $('#currencySelector').submit(function() {
     event.preventDefault();
     const currencyCode = $('#currency').val();
-    console.log(currencyCode);
+    conversionCall(currencyCode);
   });
   
 });
