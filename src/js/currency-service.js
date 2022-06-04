c7649a7ff2c29cf9daedd3951cc7cc7ec91827fa
Megan McKissack <meganmckissack@gmail.com>
2022-06-04 12:12:42 -0700

    remove try catch and test for inaccurate country code and missing api key

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,14 +1,7 @@
 export class CurrencyService {
   static async convertCurrency(currencyCode) {
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/pair/USD/${currencyCode}`);
-      if(!response.ok) {
-        throw Error(response.statusText);
-      } return response.json();
-    } catch(error) {
-      console.log(error.response["error-type"]);
-      return error.toString();
-    }
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/AAA/${currencyCode}`);
+    return response.json();
   } 
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,16 @@ import { CurrencyService } from './currency-service.js';
 
 async function conversionCall(currencyCode) {
   const response = await CurrencyService.convertCurrency(currencyCode);
+  getData(response);
   console.log(response);
+}
+
+function getData(response) {
+  if(response.result !== "success") {
+    console.log(response["error-type"]);
+  } else {
+    console.log(response["conversion_rate"]);
+  }
 }
 
 $(document).ready(function() {
