b559f29b066744f054ce58c3625c6c7a08a8fb8e
Megan McKissack <meganmckissack@gmail.com>
2022-06-10 15:51:07 -0700

    fixed file pathways due to updated file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './../css/styles.css';
-import { CurrencyService } from './currency-service.js';
+import './css/styles.css';
+import { CurrencyService } from './js/currency-service.js';
 
 
 (function() {
