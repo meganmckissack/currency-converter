47b257023d1051f4509309e3c2d093a50e235b4c
Megan McKissack <meganmckissack@gmail.com>
2022-06-10 15:37:17 -0700

    update currency service with successful error handling

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,8 +1,14 @@
 export class CurrencyService {
   static async convertCurrency(currencyCode) {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyCode}`);
-    return response.json();
-  } 
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyCode}`);
+      if(response.status !== 200 && response === 403) {
+        throw Error(response.statusText);
+      } else {
+        return response.json();
+      }
+    } catch(error) {
+      return error.response;
+    }
+  }
 }
-
-
